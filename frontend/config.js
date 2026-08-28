584dd08dd532752c4c7fe7cc05e3fc9e90fdb060
shedybaba <shedrackyashim@gmail.com>
2026-08-28 03:07:07 +0100

    updated config api

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // Change this to your deployed Railway backend URL once you have it.
 // Example: "https://task-api-backend-production.up.railway.app"
-const API_URL = "https://your-actual-url.up.railway.app";
+const API_URL = "skillful-upliftment-production-b745.up.railway.app";
